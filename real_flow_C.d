96bdf82c9102e4aa7a6bba96baeba49a4c70a465
Parfenov_Petr_Evgen'evich <parfenovpeter@master.cherenkov>
2016-07-30 13:59:04 +0300

    Cherenkov first commit

---
+++ real_flow_C.d
@@ -0,0 +1,62 @@
+
+# DO NOT DELETE
+
+./real_flow_C.so: real_flow.h variables.h /usr/include/root/TH2.h
+./real_flow_C.so: /usr/include/root/TH1.h /usr/include/root/TAxis.h
+./real_flow_C.so: /usr/include/root/TNamed.h /usr/include/root/TObject.h
+./real_flow_C.so: /usr/include/root/Rtypes.h /usr/include/root/RConfig.h
+./real_flow_C.so: /usr/include/root/RVersion.h /usr/include/root/DllImport.h
+./real_flow_C.so: /usr/include/root/Rtypeinfo.h /usr/include/root/snprintf.h
+./real_flow_C.so: /usr/include/root/strlcpy.h
+./real_flow_C.so: /usr/include/root/TGenericClassInfo.h
+./real_flow_C.so: /usr/include/root/TSchemaHelper.h
+./real_flow_C.so: /usr/include/root/TStorage.h
+./real_flow_C.so: /usr/include/root/TVersionCheck.h
+./real_flow_C.so: /usr/include/root/Riosfwd.h /usr/include/root/TBuffer.h
+./real_flow_C.so: /usr/include/root/TString.h /usr/include/root/TMathBase.h
+./real_flow_C.so: /usr/include/root/TAttAxis.h /usr/include/root/TArrayD.h
+./real_flow_C.so: /usr/include/root/TArray.h /usr/include/root/TAttLine.h
+./real_flow_C.so: /usr/include/root/TAttFill.h /usr/include/root/TAttMarker.h
+./real_flow_C.so: /usr/include/root/TArrayC.h /usr/include/root/TArrayS.h
+./real_flow_C.so: /usr/include/root/TArrayI.h /usr/include/root/TArrayF.h
+./real_flow_C.so: /usr/include/root/Foption.h /usr/include/root/TVectorFfwd.h
+./real_flow_C.so: /usr/include/root/TVectorDfwd.h
+./real_flow_C.so: /usr/include/root/TFitResultPtr.h
+./real_flow_C.so: /usr/include/root/TMatrixFBasefwd.h
+./real_flow_C.so: /usr/include/root/TMatrixDBasefwd.h
+./real_flow_C.so: /usr/include/root/TStyle.h /usr/include/root/TAttText.h
+./real_flow_C.so: /usr/include/root/TCanvas.h /usr/include/root/TPad.h
+./real_flow_C.so: /usr/include/root/TVirtualPad.h /usr/include/root/TAttPad.h
+./real_flow_C.so: /usr/include/root/TVirtualX.h /usr/include/root/GuiTypes.h
+./real_flow_C.so: /usr/include/root/Buttons.h /usr/include/root/TQObject.h
+./real_flow_C.so: /usr/include/root/TAttBBox2D.h
+./real_flow_C.so: /usr/include/root/TAttCanvas.h
+./real_flow_C.so: /usr/include/root/TCanvasImp.h
+./real_flow_C.so: /usr/include/root/TLorentzVector.h
+./real_flow_C.so: /usr/include/root/TMath.h /usr/include/root/TError.h
+./real_flow_C.so: /usr/include/root/TVector3.h /usr/include/root/TVector2.h
+./real_flow_C.so: /usr/include/root/TMatrix.h /usr/include/root/TMatrixF.h
+./real_flow_C.so: /usr/include/root/TMatrixT.h
+./real_flow_C.so: /usr/include/root/TMatrixTBase.h
+./real_flow_C.so: /usr/include/root/TMatrixTUtils.h
+./real_flow_C.so: /usr/include/root/TMatrixFfwd.h
+./real_flow_C.so: /usr/include/root/TMatrixFUtils.h
+./real_flow_C.so: /usr/include/root/TMatrixFUtilsfwd.h
+./real_flow_C.so: /usr/include/root/TRotation.h /usr/include/root/TProfile.h
+./real_flow_C.so: /usr/include/root/TROOT.h /usr/include/root/TDirectory.h
+./real_flow_C.so: /usr/include/root/TList.h
+./real_flow_C.so: /usr/include/root/TSeqCollection.h
+./real_flow_C.so: /usr/include/root/TCollection.h
+./real_flow_C.so: /usr/include/root/TIterator.h /usr/include/root/TDatime.h
+./real_flow_C.so: /usr/include/root/TUUID.h /usr/include/root/TChain.h
+./real_flow_C.so: /usr/include/root/TTree.h /usr/include/root/TBranch.h
+./real_flow_C.so: /usr/include/root/TObjArray.h /usr/include/root/TDataType.h
+./real_flow_C.so: /usr/include/root/TDictionary.h
+./real_flow_C.so: /usr/include/root/Property.h /usr/include/root/ESTLType.h
+./real_flow_C.so: /usr/include/root/TClass.h /usr/include/root/TObjString.h
+./real_flow_C.so: /usr/include/root/TVirtualTreePlayer.h
+./real_flow_C.so: /usr/include/root/TFile.h
+./real_flow_C.so: /usr/include/root/TDirectoryFile.h /usr/include/root/TMap.h
+./real_flow_C.so: /usr/include/root/THashTable.h /usr/include/root/TUrl.h
+./real_flow_C.so: /usr/include/root/cintdictversion.h /usr/include/root/RVersion.h
+real_flow_C__ROOTBUILDVERSION= 5.34/19
